5c6f774caaabfd2122d63ce1aaa431e28d18730e
Leslie <lehouxiu@gmail.com>
2014-10-30 11:59:50 +0800

    规范文件命名

--- js/calendar.js
+++ js/calendar.js
@@ -1,21 +1,4 @@
 /**
- * reverse dom of a parentNode children
- * @param parent parentNode
- * @return undefined
- */
-function reverseDom (parent) {
-	var nodes = parent.childNodes, cur, newNode = document.createDocumentFragment();
-	for (var len=nodes.length, i=len; i; i--) {
-		cur = nodes[i - 1];
-		if(cur.nodeType === 1) {
-			newNode.appendChild(cur);
-		}
-	};
-	parent.innerHTML = '';
-	parent.appendChild(newNode);
-}
-
-/**
  * get Element by class
  * @return {HTMLCollection} target elements
  */
@@ -312,17 +295,3 @@ function initCalendar (opt) {
 		bodyEle.innerHTML = '<table>' + bodyStr + '</table>';
 	}
 }
-
-(function () {
-	if (window.getComputedStyle) {
-		//morden
-		window.getStyle = function (obj, style) {
-			return window.getComputedStyle(obj, false)[style];
-		}
-	} else {
-		//ie
-		window.getStyle = function (obj, style) {
-			return obj.currentStyle[style];
-		}
-	}
-})()
